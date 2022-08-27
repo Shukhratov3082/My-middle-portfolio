df8c81aa9baa0c548bf5efb53b9169b34d888596
Sorrybro802 <poweshuxratov@gmail.com>
2022-08-27 22:41:44 +0500

    ko'rinish tayyor

---
+++ script.js
@@ -0,0 +1,38 @@
+window.addEventListener('DOMContentLoaded', function () {
+  window.addEventListener('scroll', function() {
+    const header = document.querySelector('header');
+    header.classList.toggle('sticky', window.scrollY > 0)
+  })
+
+  const menuBtn = document.querySelector('.menu-btn')
+  const navigation = document.querySelector('.navigation')
+  const navigationItems = document.querySelectorAll('.navigation a')
+
+  menuBtn.addEventListener('click', () => {
+    menuBtn.classList.toggle('active')
+    navigation.classList.toggle('active')
+  })
+
+  navigationItems.forEach(navItem => {
+    navItem.addEventListener('click', () => {
+      menuBtn.classList.remove('active')
+      navigation.classList.remove('active')
+    })
+  })
+
+ 
+
+  window.addEventListener('scroll', () => {
+    let reveals = document.querySelectorAll('.reveal')
+
+    for(let i = 0; i< reveals.length; i++) {
+      let windowHeight = window.innerHeight;
+      let revealTop = reveals[i].getBoundingClientRect().top;
+      let revealPoint = 50;
+
+      if(revealTop < windowHeight - revealPoint) {
+        reveals[i].classList.add('active')
+      }
+    }
+  })
+})
